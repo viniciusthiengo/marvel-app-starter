f040c0967baf02099a27c979266215285e9ca727
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-22 12:07:47 -0300

    Updating and expanding the model classes area.

---
+++ app/src/main/java/vini/lop/io/marvelappstarter/data/model/character/CharacterModel.kt
@@ -0,0 +1,14 @@
+package vini.lop.io.marvelappstarter.data.model.character
+
+import android.os.Parcelable
+import com.google.gson.annotations.SerializedName
+import kotlinx.parcelize.Parcelize
+import vini.lop.io.marvelappstarter.data.model.ThumbnailModel
+
+@Parcelize
+class CharacterModel(
+    @SerializedName("id") val id: Int,
+    @SerializedName("name") val name: String,
+    @SerializedName("description") val description: String,
+    @SerializedName("thumbnail") val thumbnailModel: ThumbnailModel
+) : Parcelable

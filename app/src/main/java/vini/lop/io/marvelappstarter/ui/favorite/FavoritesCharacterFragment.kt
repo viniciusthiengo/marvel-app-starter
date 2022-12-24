4302ea336b1b7a4d76b52c125403eadfe6db034d
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-24 16:48:59 -0300

    Code style refactoring.

--- app/src/main/java/vini/lop/io/marvelappstarter/ui/favorite/FavoritesCharacterFragment.kt
+++ app/src/main/java/vini/lop/io/marvelappstarter/ui/favorite/FavoritesCharacterFragment.kt
@@ -3,18 +3,18 @@ package vini.lop.io.marvelappstarter.ui.favorite
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.fragment.app.viewModels
-import vini.lop.io.marvelappstarter.databinding.FragmentFavoriteCharacterBinding
+import vini.lop.io.marvelappstarter.databinding.FragmentFavoritesCharacterBinding
 import vini.lop.io.marvelappstarter.ui.base.BaseFragment
 
 class FavoritesCharacterFragment :
-    BaseFragment<FragmentFavoriteCharacterBinding, FavoritesCharacterViewModel>() {
+    BaseFragment<FragmentFavoritesCharacterBinding, FavoritesCharacterViewModel>() {
     override val viewModel: FavoritesCharacterViewModel by viewModels()
 
     override fun getViewBinding(
         inflater: LayoutInflater,
         container: ViewGroup?
-    ): FragmentFavoriteCharacterBinding =
-        FragmentFavoriteCharacterBinding.inflate(
+    ): FragmentFavoritesCharacterBinding =
+        FragmentFavoritesCharacterBinding.inflate(
             inflater,
             container,
             false

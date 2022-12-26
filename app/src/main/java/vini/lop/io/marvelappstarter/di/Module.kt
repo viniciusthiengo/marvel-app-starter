875c58977ca24fe6881d4ff0ca7b5d295055677b
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-26 10:23:39 -0300

    Creating the Hilt app unique module.

---
+++ app/src/main/java/vini/lop/io/marvelappstarter/di/Module.kt
@@ -0,0 +1,39 @@
+package vini.lop.io.marvelappstarter.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import vini.lop.io.marvelappstarter.data.remote.ServiceApi
+import vini.lop.io.marvelappstarter.util.Constants.BASE_URL
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        TODO()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(client)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi {
+        return retrofit.create(ServiceApi::class.java)
+    }
+}

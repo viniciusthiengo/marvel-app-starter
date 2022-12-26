f98dfb53d34075ce07e76a9ab76f28a0a5619a1e
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-26 10:55:27 -0300

    Adding the whole OkHttp intercept code.

--- app/src/main/java/vini/lop/io/marvelappstarter/di/Module.kt
+++ app/src/main/java/vini/lop/io/marvelappstarter/di/Module.kt
@@ -5,10 +5,20 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import timber.log.Timber
 import vini.lop.io.marvelappstarter.data.remote.ServiceApi
 import vini.lop.io.marvelappstarter.util.Constants.BASE_URL
+import vini.lop.io.marvelappstarter.util.Constants.PARAM_API_KEY
+import vini.lop.io.marvelappstarter.util.Constants.PARAM_HASH
+import vini.lop.io.marvelappstarter.util.Constants.PARAM_TS
+import vini.lop.io.marvelappstarter.util.Constants.PRIVATE_KEY
+import vini.lop.io.marvelappstarter.util.Constants.PUBLIC_KEY
+import java.math.BigInteger
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
 import javax.inject.Singleton
 
 @Module
@@ -18,7 +28,65 @@ object Module {
     @Singleton
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
-        TODO()
+        val logging = HttpLoggingInterceptor().apply {
+            level = HttpLoggingInterceptor.Level.BODY
+        }
+
+        return OkHttpClient()
+            .newBuilder()
+            .addInterceptor { chain ->
+                val currentTimestamp = System.currentTimeMillis()
+
+                val newUrl = chain
+                    .request()
+                    .url
+                    .newBuilder()
+                    .addQueryParameter(
+                        PARAM_TS,
+                        currentTimestamp.toString()
+                    )
+                    .addQueryParameter(
+                        PARAM_API_KEY,
+                        PUBLIC_KEY
+                    )
+                    .addQueryParameter(
+                        PARAM_HASH,
+                        provideToMd5Hash(encrypted = "$currentTimestamp$PRIVATE_KEY$PUBLIC_KEY")
+                    )
+                    .build()
+
+                val newRequest = chain
+                    .request()
+                    .newBuilder()
+                    .url(newUrl)
+                    .build()
+
+                chain.proceed(newRequest)
+            }
+            .addInterceptor(logging)
+            .build()
+    }
+
+    private fun provideToMd5Hash(encrypted: String): String {
+        var pass = encrypted
+        var encryptedString: String? = null
+        val md5: MessageDigest
+
+        try {
+            md5 = MessageDigest.getInstance("MD5")
+            md5.update(pass.toByteArray(), 0, pass.length)
+            pass = BigInteger(1, md5.digest()).toString(16)
+            while (pass.length < 32) {
+                pass = "0$pass"
+            }
+            encryptedString = pass
+        } catch (e1: NoSuchAlgorithmException) {
+            e1.printStackTrace()
+        }
+
+        Timber.d("hash -> $encryptedString")
+
+        return encryptedString ?: ""
     }
 
     @Singleton
--- app/src/main/java/vini/lop/io/marvelappstarter/util/Constants.kt
+++ app/src/main/java/vini/lop/io/marvelappstarter/util/Constants.kt
@@ -6,4 +6,8 @@ object Constants {
 
     const val PUBLIC_KEY = "c27ba9623568bb8904fb23b0f9f7bf94"
     const val PRIVATE_KEY = "9b96ee39dd2c178bfc7409a4af38ec300b94c40a"
+
+    const val PARAM_TS = "ts"
+    const val PARAM_API_KEY = "apikey"
+    const val PARAM_HASH = "hash"
 }

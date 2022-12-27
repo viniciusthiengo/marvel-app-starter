c80748c82d2495e76054b9f1a124ea3ee3aea8eb
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-27 11:03:10 -0300

    Adding a new adapter for all character lists on the UI side.

---
+++ app/src/main/java/vini/lop/io/marvelappstarter/ui/adapters/CharacterAdapter.kt
@@ -0,0 +1,88 @@
+package vini.lop.io.marvelappstarter.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.AsyncListDiffer
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import vini.lop.io.marvelappstarter.R
+import vini.lop.io.marvelappstarter.data.model.character.CharacterModel
+import vini.lop.io.marvelappstarter.databinding.ItemCharacterBinding
+import vini.lop.io.marvelappstarter.util.preciseSubstring
+
+class CharacterAdapter : RecyclerView.Adapter<CharacterAdapter.CharacterViewHolder>() {
+
+    private val differCallback = object : DiffUtil.ItemCallback<CharacterModel>() {
+        override fun areItemsTheSame(
+            oldItem: CharacterModel,
+            newItem: CharacterModel
+        ): Boolean {
+            return oldItem.hashCode() == newItem.hashCode()
+        }
+
+        override fun areContentsTheSame(
+            oldItem: CharacterModel,
+            newItem: CharacterModel
+        ): Boolean {
+            return oldItem == newItem
+        }
+    }
+
+    private val differ = AsyncListDiffer(this, differCallback)
+
+    var characters: List<CharacterModel>
+        get() = differ.currentList
+        set(value) = differ.submitList(value)
+
+    inner class CharacterViewHolder(
+        val binding: ItemCharacterBinding
+    ) : RecyclerView.ViewHolder(binding.root)
+
+    override fun getItemCount(): Int = characters.size
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): CharacterViewHolder =
+        CharacterViewHolder(
+            ItemCharacterBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+
+    override fun onBindViewHolder(
+        holder: CharacterViewHolder,
+        position: Int
+    ) {
+        val character = characters[position]
+
+        holder.binding.apply {
+            tvNameCharacter.text = character.name
+
+            tvDescriptionCharacter.text = if (character.description.trim().isEmpty()) {
+                holder.itemView.context.getString(R.string.text_description_empty)
+            } else {
+                character.description.preciseSubstring(100)
+            }
+
+            Glide.with(holder.itemView.context)
+                .load(character.thumbnailModel.getFullPath())
+                .into(imgCharacter)
+        }
+
+        holder.itemView.setOnClickListener {
+            onItemClickListener?.let {
+                it(character)
+            }
+        }
+    }
+
+    private var onItemClickListener: ((CharacterModel) -> Unit)? = null
+
+    fun setOnClickListener(listener: (CharacterModel) -> Unit) {
+        onItemClickListener = listener
+    }
+}

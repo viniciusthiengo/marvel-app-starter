aaba3cc8f33665a904f42ae30a91a1f54206b73c
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-28 10:44:41 -0300

    Code style refactoring.

--- app/src/main/java/vini/lop/io/marvelappstarter/ui/state/ResourceState.kt
+++ app/src/main/java/vini/lop/io/marvelappstarter/ui/state/ResourceState.kt
@@ -5,7 +5,13 @@ sealed class ResourceState<T>(
     val message: String? = null
 ) {
     class Success<T>(data: T) : ResourceState<T>(data)
-    class Error<T>(message: String, data: T? = null) : ResourceState<T>(data, message)
+
+    class Error<T>(
+        message: String,
+        data: T? = null
+    ) : ResourceState<T>(data, message)
+
     class Loading<T> : ResourceState<T>()
+
     class Empty<T> : ResourceState<T>()
 }

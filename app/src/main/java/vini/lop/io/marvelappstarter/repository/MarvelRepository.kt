93e6957dca3408f153286d12c37a2e84765409d0
Vinícius Thiengo <viniciusthiengo@gmail.com>
2022-12-28 10:48:56 -0300

    Adding the repository (the one that makes it possible to take all the needed remote data).

---
+++ app/src/main/java/vini/lop/io/marvelappstarter/repository/MarvelRepository.kt
@@ -0,0 +1,15 @@
+package vini.lop.io.marvelappstarter.repository
+
+import vini.lop.io.marvelappstarter.data.remote.ServiceApi
+import javax.inject.Inject
+
+class MarvelRepository @Inject constructor(
+    private val api: ServiceApi
+) {
+
+    suspend fun list(nameStartsWith: String? = null) =
+        api.list(nameStartsWith)
+
+    suspend fun getComics(characterId: Int) =
+        api.getComics(characterId)
+}

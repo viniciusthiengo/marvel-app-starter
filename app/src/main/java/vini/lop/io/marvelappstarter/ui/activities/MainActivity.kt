8a711c9e535062eb0f91cb04590eef05f124a023
Vinícius Thiengo <viniciusthiengo@gmail.com>
2023-01-01 14:55:43 -0300

    Adding the Splash screen configuration and also adding some code style refactoring.

--- app/src/main/java/vini/lop/io/marvelappstarter/ui/activities/MainActivity.kt
+++ app/src/main/java/vini/lop/io/marvelappstarter/ui/activities/MainActivity.kt
@@ -1,10 +1,11 @@
-package vini.lop.io.marvelappstarter
+package vini.lop.io.marvelappstarter.ui.activities
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
+import vini.lop.io.marvelappstarter.R
 import vini.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 @AndroidEntryPoint
